43e99cf39b72deba94eb64bfbecbcf1e2ba31f28
birkoss <admin@birkoss.com>
2016-11-15 12:43:05 -0500

    Adding a DataManager to handle savegame

---
+++ Assets/Scripts/Managers/DataManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using UnityEngine;
+
+public class DataManager : Singleton<DataManager> {
+
+    public void Save() {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "save.dat");
+
+        SaveData data = new SaveData();
+        data.health = 10;
+
+        bf.Serialize(file, data);
+        file.Close();
+    }
+
+    public void Load() {
+        if (File.Exists(Application.persistentDataPath + "save.dat")) {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "save.dat", FileMode.Open);
+
+            SaveData data = (SaveData)bf.Deserialize(file);
+
+            file.Close();
+
+            Debug.Log(data.health);
+        }
+    }
+}
+
+[System.Serializable]
+class SaveData {
+    public int health;
+}
